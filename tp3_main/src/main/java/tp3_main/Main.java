65dcc4282d5dec93b8de18c38aea93d34f7edd39
thmsbtn <thomas.beatini@gmail.com>
2019-03-12 08:05:09 +0100

    test de URLclassLoader

--- tp3_main/src/main/java/tp3_main/Main.java
+++ tp3_main/src/main/java/tp3_main/Main.java
@@ -1,18 +1,31 @@
 package tp3_main;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 public class Main {
 
-	public static void main(String[] args) throws MalformedURLException {
+	public static void main(String[] args) throws MalformedURLException{
 		// TODO Auto-generated method stub
 		URL[] url = new URL[1];
-		url[0] = new URL("./tp3_test");
+		url[0] = new URL("file:///~/Documents/MIAGE/Programmation_Av/tp-thmsbtn/tp3_test");
+		String classTest = "target/classes/tp3_test/Test.class";
+		
+		
 		
 		URLClassLoader urlCL = new URLClassLoader(url);
 		
+		
+		try {
+			urlCL.loadClass(classTest);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Main.main()"+urlCL.getClass());
 		
 	}
 
